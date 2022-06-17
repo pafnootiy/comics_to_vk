90d6f1f38a6a839d33a271024d5e8933ca682e83
pafnootiy <ksander-86@mail.ru>
2022-06-17 15:25:11 +0300

    fix VKError HTTPerror cheking

--- post_commics.py
+++ post_commics.py
@@ -1,25 +1,29 @@
 import os
 import random
-import requests
 from pathlib import Path
+
+import requests
 from dotenv import load_dotenv
-from pprint import pprint
+
+
+class VKError(Exception):
+    pass
 
 
 def get_random_comics_xkcd_number():
     random_comics_url = "https://xkcd.com/info.0.json"
-    random_comics = requests.get(random_comics_url)
-    random_comics.raise_for_status()
-    last_comics_number = random_comics.json()['num']
+    response = requests.get(random_comics_url)
+    response.raise_for_status()
+    last_comics_number = response.json()['num']
     comics_number = random.randint(1, last_comics_number)
     return comics_number
 
 
 def get_url_and_comment_from_xkcd(comics_number):
     url_xkcd = f"https://xkcd.com/{comics_number}/info.0.json"
-    request_xkcd = requests.get(url_xkcd)
-    request_xkcd.raise_for_status()
-    respone_xkcd = request_xkcd.json()
+    response = requests.get(url_xkcd)
+    response.raise_for_status()
+    respone_xkcd = response.json()
     comics_url = respone_xkcd["img"]
     comics_comment = respone_xkcd['alt']
     return comics_url, comics_comment
@@ -32,7 +36,6 @@ def save_comics_picture(image_url, picture_pass):
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
-        file.close()
 
 
 def check_vk_api(vk_response):
@@ -47,11 +50,11 @@ def upload_picture_to_vk_server(token, group_id, path_to_pic):
         "group_id": group_id,
         "v": 5.131
     }
-    upload_server_response = requests.get(upload_server_url, params=payload_upload)
-    upload_server_response.raise_for_status()
-    upload_server_response_json = upload_server_response.json()
-    url_for_upload_comics = upload_server_response_json['response']['upload_url']
-    check_vk_api(upload_server_response_json)
+    response = requests.get(upload_server_url, params=payload_upload)
+    response.raise_for_status()
+    upload_server_response = response.json()
+    url_for_upload_comics = upload_server_response['response']['upload_url']
+    check_vk_api(upload_server_response)
     with open(path_to_pic, 'rb') as file:
         files = {
             'photo': file,
@@ -118,7 +121,7 @@ def main():
         image_id = save_picture_vk(uploading_comics, comics_comment, vk_token, group_id, user_id)
         get_public_comics_on_the_wall(image_id, comics_comment, vk_token, group_id)
 
-    except HTTPError:
+    except requests.HTTPError:
         print("ошибка HTTPError")
     except VKError:
         print("ошибка VKError")
